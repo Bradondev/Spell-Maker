3b9100525d784a71dd0f32beda68506021e94359
Brandon Martin Del Campo <brandondevprog@gmail.com>
2023-11-16 21:59:50 -0800

    Files can now be read , fixed a bug that happened when you make a new dir

--- app.go
+++ app.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
+	
 )
 
 type Spell struct{
@@ -28,20 +30,6 @@ type SpellStats struct{
  var NewSpell Spell
  var NameOfSpellsInFolder []string 
 // App struct
-
-
-func MakeNewDirectory(NewSpellType string){
-	_, err := os.Open(NewSpellType)
-	if err != nil {
-	if os.IsNotExist(err) {
-		directoryname := NewSpellType
-		err2 := os.Mkdir(directoryname, 0755) //create a directory and give it required permissions
-		if err2 != nil {
-		fmt.Println(err2) //print the error on the console
-		return}}}
-	SaveSpellToFolder()
-	fmt.Println("This folder has already been made")
-	}
 
 
 
@@ -76,12 +64,37 @@ func (a *App) startup(ctx context.Context) {
 	//
 		for _, v := range files {
 			NameOfSpellsInFolder = append(NameOfSpellsInFolder,v.Name())
-			fmt.Println(v.Name(), v.IsDir())
+			fmt.Println(v.Name())
+			if v.IsDir(){
+				fmt.Println("open dir")
+				n, err := os.Open("Spells/"+v.Name())
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println("Open new text file")
+				SpellFiles, err := n.Readdir(0)
+				if err != nil {
+				fmt.Println(err)
+				return}
+				for _, P := range SpellFiles {
+					file, err := os.Open("Spells/"+v.Name()+"/"+P.Name())
+					if err != nil {
+						fmt.Println(err)
+					}
+					b, err := io.ReadAll(file)
+					fmt.Print(string(b))
+					
+				 }
+
+	////
+				fmt.Println(v.Name() +" is a dir")
+			}
 		}
 		fmt.Println(NameOfSpellsInFolder)
 	
 }
-
+////
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
@@ -118,6 +131,21 @@ func  SaveSpellToFolder()  {
 	}
 }
 
+func MakeNewDirectory(NewSpellType string){
+	_, err := os.Open(NewSpellType)
+	if err != nil {
+	if os.IsNotExist(err) {
+		directoryname := "Spells/"+NewSpellType
+		err2 := os.Mkdir(directoryname, 0755) //create a directory and give it required permissions
+		if err2 != nil {
+		SaveSpellToFolder()
+		fmt.Println(err2)
+		 //print the error on the console
+		return}}}
+	SaveSpellToFolder()
+	fmt.Println("This folder has already been made")
+	}
+//
 
 
 
